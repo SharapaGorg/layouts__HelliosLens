e012495c838413505cc7cd7b64312edf42033a35
SharapaGorg <sharapov.savely@yandex.ru>
2021-12-05 14:01:42 +0300

    first

---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+
+    <nav>
+
+    </nav>
+
+    <div class = 'mainContainer'>
+      <nuxt/>
+    </div>
+
+    <footer>
+
+
+    </footer>
+
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+<!--    <center>-->
+      <div class = 'mainDisplay'>
+        <span class = 'bigText'>Legendary Helios lens</span>
+        <span class = 'monsterBokeh'>MONSTER BOKEH</span>
+      </div>
+<!--    </center>-->
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
