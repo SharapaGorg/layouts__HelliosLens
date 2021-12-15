4eb94bc5a89e2a2ddfc1c69dcf8f1ff9883cc5c1
SharapaGorg <sharapov.savely@yandex.ru>
2021-12-15 20:08:28 +0300

    add technical characteristics and images

---
+++ components/technicalCharacteristic.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class = '_container' ref = 'main'>
+    <img :src="resolve_img_url(svgPath)">
+    <div class = 'tech-title' ref = 'title'>{{title}}</div>
+    <div class = 'tech-description' ref = 'desc'>{{description}}</div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+
+    }
+  },
+  props : ['title', 'description', 'svgPath', 'titleW'],
+  methods : {
+    resolve_img_url: function (path) {
+      let images = require.context('../static/', false, /\.png$|\.jpg$|\.svg$/)
+      return images("./"+path)
+    }
+  },
+  mounted() {
+    const container = this.$refs.main;
+    if (this.titleW) {
+        this.$refs.title.style.width = this.titleW + 'px'
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+
+._container {
+  /*height : 222px;*/
+  width : 211px;
+  /*display : inline;*/
+}
+
+.tech-title {
+  width: 183px;
+  height: 54px;
+  position: relative;
+  top : 35.5px;
+
+  font-family: Open Sans,serif;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 18px;
+  line-height: 150%;
+
+  text-align: center;
+  color: #000000;
+}
+
+.tech-description {
+  position : relative;
+  top : 50px;
+  width: 211px;
+  height: 42px;
+
+  font-family: Open Sans,serif;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 16px;
+  line-height: 150%;
+
+  text-align: center;
+  color: #000000;
+}
+
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -11,19 +11,42 @@
       <div class='main-content'>
         <div class='examples-inscription'>Examples of photos taken on Helios</div>
         <div class='image-box'>
-          <div>
-            <img src = '../static/image1.svg' class='image1'/>
-            <img src = '../static/image2.svg' class='image2'/>
-            <img src = '../static/image3.svg' class='image3'/>
-            <img src = '../static/image4.svg' class='image4'/>
-          </div>
-          <div>
-            <img class='image5'/>
-            <img class='image6'/>
-            <img class='image7'/>
-            <img class='image8'/>
-          </div>
+          <img src='../static/image1.svg' class='image1' ref='image'/>
+          <img src='../static/image2.svg' class='image2'/>
+          <img src='../static/image3.svg' class='image3'/>
+          <img src='../static/image4.svg' class='image4'/>
+          <img src='../static/image5.svg' class='image5'/>
+          <img src='../static/image6.svg' class='image6'/>
+          <img src='../static/image7.svg' class='image7'/>
+          <img src='../static/image8.svg' class='image8'/>
         </div>
+        <br><br><br><br><br><br><br><br><br><br><br><br>
+        <div class='examples-inscription'>Technical characteristics of Helios lens</div>
+
+        <div class='technical-characteristics'>
+          <div class='col-start-2'>
+            <technical-characteristic svg-path="tech1.svg" title='Focal length 50mm-85mm'
+                                      description="This focal length is great fot shooting portraits"/>
+          </div>
+
+          <div class='col-start-3'>
+            <technical-characteristic svg-path="tech2.svg" title='6-13 diaphragm petals'
+                                      description="In the pictures draws a remarkable blur of areas of sharpness (side)."/>
+          </div>
+
+          <div class='col-start-4'>
+            <technical-characteristic title-w="96"
+              svg-path="tech3.svg" title='Luminosil 1.5-2.0'
+                                      description="Enough for shooting in moderate lighting conditions."/>
+          </div>
+
+          <div class='col-start-5'>
+            <technical-characteristic svg-path="tech4.svg" title="Optical diagram of type 'Planar'"
+                                      description="The optical circuit was copied from the Carl Zeiss Jena Biotar 2/58 lens."/>
+          </div>
+
+        </div>
+
       </div>
 
     </center>
@@ -31,5 +54,17 @@
 </template>
 
 <script>
-export default {}
+
+import TechnicalCharacteristic from "../components/technicalCharacteristic";
+
+export default {
+  data() {
+    return {}
+  },
+  mounted() {
+  },
+  components: {
+    TechnicalCharacteristic
+  }
+}
 </script>
